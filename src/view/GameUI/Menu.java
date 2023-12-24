07fd8695c2e338f2f26689041f0719844f56ed6f
Vo Minh Khang <itcsiu21136@student.hcmiu.edu.vn>
2023-12-24 14:08:49 +0700

    resize

--- src/view/GameUI/Menu.java
+++ src/view/GameUI/Menu.java
@@ -9,26 +9,26 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
-public class Menu extends JFrame{
+public class Menu extends JFrame {
   private BufferedImage image;
+
   public Menu() throws IOException {
     this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-    this.setSize(1000, 1000);
+    this.setLocationRelativeTo(null);
+    this.setResizable(false);
+    this.setSize(874, 643);
     this.setVisible(true);
-    try {
-      image = ImageIO.read(new File("img\\Menu.png"));
-    } catch (Exception e) {
-      e.printStackTrace();    }
-
+    image = ImageIO.read(new File("img\\Menu.png"));
     JPanel pane = new JPanel() {
       @Override
       protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.drawImage(image, 0, 0, null);
+        g.drawImage(image, 5, 5, null);
       }
     };
     this.add(pane);
   }
+
   public static void main(String[] args) {
     try {
       Menu m = new Menu();
