6eb53b14485af21f323fbc5712f40565e8c41832
Vo Minh Khang <itcsiu21136@student.hcmiu.edu.vn>
2023-12-24 15:07:06 +0700

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,10 +1,10 @@
 import view.GameFrame;
-public class App {
 
+public class App {
     public static void main(String[] args) throws Exception {
         int width = 20;
         int height = 20;
-        int boom_num = 20;
+        int boom_num = 60;
         GameFrame gf = new GameFrame(width, height, boom_num);
     }
 }
