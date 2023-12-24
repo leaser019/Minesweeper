bb9974cf5070d1d8b3740076be3b5f05d1fe3ab0
Vo Minh Khang <itcsiu21136@student.hcmiu.edu.vn>
2023-12-24 13:57:20 +0700

    Update Menu.java

--- src/view/GameUI/Menu.java
+++ src/view/GameUI/Menu.java
@@ -1,3 +1,5 @@
+package view.GameUI;
+
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -7,15 +9,16 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
-public class Menu {
-
-  public Menu() {
-    JFrame frame = new JFrame();
-    frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-    frame.setSize(200, 200);
-    frame.setVisible(true);
-
-    final BufferedImage image = ImageIO.read(new File(""));
+public class Menu extends JFrame{
+  private BufferedImage image;
+  public Menu() throws IOException {
+    this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    this.setSize(1000, 1000);
+    this.setVisible(true);
+    try {
+      image = ImageIO.read(new File("img\\Menu.png"));
+    } catch (Exception e) {
+      e.printStackTrace();    }
 
     JPanel pane = new JPanel() {
       @Override
@@ -24,9 +27,13 @@ public class Menu {
         g.drawImage(image, 0, 0, null);
       }
     };
-    frame.add(pane);
+    this.add(pane);
   }
   public static void main(String[] args) {
-    Menu m = new Menu();
+    try {
+      Menu m = new Menu();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 }
